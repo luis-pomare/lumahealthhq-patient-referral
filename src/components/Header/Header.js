e343649d9d7a24ae72a0e79f7911e68ab8754695
Luis Pomare <luispomare92@gmail.com>
2023-10-03 14:45:30 -0500

    Import header from material UI

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,14 @@
-import './Header.css';
-
-import React from 'react'
+import { AppBar, Toolbar } from "@mui/material";
+import React from "react";
 
 export default function Header() {
+  const displayDesktop = () => {
+    return <Toolbar>Hi From Desktop Header</Toolbar>;
+  };
+
   return (
-    <nav>Header</nav>
-  )
+    <header>
+      <AppBar>{displayDesktop()}</AppBar>
+    </header>
+  );
 }
